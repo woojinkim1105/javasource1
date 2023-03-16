47a82b523db9e290fb5541dbda62a773e8718808
woojin <asdf961105@gmail.com>
2023-03-16 17:36:51 +0900

    SimpleDateFormat 주석 추가

--- ch7/src/util/SimpleDateFormatEx1.java
+++ ch7/src/util/SimpleDateFormatEx1.java
@@ -13,8 +13,9 @@ public class SimpleDateFormatEx1 {
 
 	public static void main(String[] args) {
 		
+		// 시스템 날짜와 시간 가져오기
 		Date date = new Date();
-		
+		System.out.println(date);
 		
 		
 		SimpleDateFormat sdf = new SimpleDateFormat();
